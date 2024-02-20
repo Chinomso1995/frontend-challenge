8efac3f13f89f216432428ed5054b5fa15ab6f26
unknown <chinomsojohnson@gmail.com>
2024-02-20 13:28:58 -0300

    finalising stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,6 @@ import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
-import Seo from "../components/seo"
-import * as styles from "../components/index.module.css"
 import MenuTab from "../components/menutab"
 
 const IndexPage = () => (
